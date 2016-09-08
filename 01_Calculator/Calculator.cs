92eddd6e435966ec41ef24fbd7b10f2a3cebfff9
Kast <Kast@DESKTOP-OEB230F>
2016-09-08 09:23:04 +0200

    Add project files.

---
+++ 01_Calculator/Calculator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace _01_Calculator
+{
+    internal class Calculator
+    {
+        internal static int Add(int v1, int v2)
+        {
+            return v1 + v2;
+        }
+
+        internal static int Subtract(int v1, int v2)
+        {
+            return v1 - v2;
+        }
+
+        internal static int Sum(int[] numbers)
+        {
+            int sum = 0;
+            /*for(int i = 0; i<numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }*/
+            foreach(int number in numbers)
+            {
+                sum += number;
+            }
+            return sum;
+        }
+
+        internal static int Multiply(int[] numbers)
+        {
+            int mult = 1;
+            /*for(int i = 0; i<numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }*/
+            if (numbers.Length <= 0)
+                return 0;
+
+            foreach (int number in numbers)
+            {
+                mult *= number;
+            }
+            return mult;
+        }
+
+        internal static int Power(int v1, int v2)
+        {
+            return (int) Math.Pow(v1,v2);
+        }
+
+        internal static int Factorial(int v)
+        {
+            int factSum = 1;
+            for(int i = v; i>0; i--)
+            {
+                factSum *= i;
+            }
+            return factSum;
+        }
+    }
+}
---
+++ 01_Calculator/CalculatorTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _01_Calculator
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void Add()
+        {
+            Assert.AreEqual(0, Calculator.Add(0, 0));
+            Assert.AreEqual(4, Calculator.Add(2, 2));
+            Assert.AreEqual(8, Calculator.Add(2, 6));
+        }
+        [TestMethod]
+        public void Subtract()
+        {
+            Assert.AreEqual(6, Calculator.Subtract(10, 4));
+        }
+        [TestMethod]
+        public void Sum()
+        {
+            //Arrange
+            int[] numbers = new int[] { };
+            Assert.AreEqual(0, Calculator.Sum(numbers));
+
+            numbers = new int[1];
+            numbers[0] = 7;
+            Assert.AreEqual(7, Calculator.Sum(numbers));
+
+            numbers = new int[2];
+            numbers[0] = 7;
+            numbers[1] = 11;
+            Assert.AreEqual(18, Calculator.Sum(numbers));
+
+        }
+        [TestMethod]
+        public void Multiply()
+        {
+            int[] numbers = new int[] { };
+            Assert.AreEqual(0, Calculator.Multiply(numbers));
+
+            numbers = new int[1];
+            numbers[0] = 7;
+            Assert.AreEqual(7, Calculator.Multiply(numbers));
+
+            numbers = new int[2];
+            numbers[0] = 7;
+            numbers[1] = 11;
+            Assert.AreEqual(77, Calculator.Multiply(numbers));
+        }
+        [TestMethod]
+        public void Power()
+        {
+            Assert.AreEqual(1, Calculator.Power(1, 2));
+            Assert.AreEqual(4, Calculator.Power(2, 2));
+            Assert.AreEqual(27, Calculator.Power(3, 3));
+        }
+        [TestMethod]
+        public void Factorial()
+        {
+            Assert.AreEqual(120, Calculator.Factorial(5));
+            Assert.AreEqual(3628800, Calculator.Factorial(10));
+        }
+    }
+}
