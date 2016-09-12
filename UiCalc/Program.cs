8df503525a333540894f76f570464b4e0b99a90e
davi7725 <davi7725@edu.eal.dk>
2016-09-12 11:35:25 +0200

    Added a ConstructExpression method so it writes the whole expression to the console.

--- UiCalc/Program.cs
+++ UiCalc/Program.cs
@@ -16,8 +16,8 @@ namespace UiCalc
             do
             {
                 Console.Clear();
-                Console.WriteLine("1 - Add \n2 - Subtract \n3 - Sum \n4 - Multiply \n5 - Power \n6 - Factorial \n7 - EXIT");
-                Console.WriteLine("Please choose an option: ");
+                PrintMenu();
+                
                 op = Console.ReadLine();
                 Console.Clear();
                 int[] valuesForOperations;
@@ -26,27 +26,27 @@ namespace UiCalc
                 {
                     case "1":
                         valuesForOperations = AskNumbers(2);
-                        ShowResult(Calculator.Add(valuesForOperations[0], valuesForOperations[1]));
+                        ShowResult(Calculator.Add(valuesForOperations[0], valuesForOperations[1]), valuesForOperations, "+");
                         break;
                     case "2":
                         valuesForOperations = AskNumbers(2);
-                        ShowResult(Calculator.Subtract(valuesForOperations[0], valuesForOperations[1]));
+                        ShowResult(Calculator.Subtract(valuesForOperations[0], valuesForOperations[1]), valuesForOperations, "-");
                         break;
                     case "3":
                         valuesForOperations = AskNumbers(AskAmountOfOperationNumbers());
-                        ShowResult(Calculator.Sum(valuesForOperations));
+                        ShowResult(Calculator.Sum(valuesForOperations), valuesForOperations, "+");
                         break;
                     case "4":
                         valuesForOperations = AskNumbers(AskAmountOfOperationNumbers());
-                        ShowResult(Calculator.Multiply(valuesForOperations));
+                        ShowResult(Calculator.Multiply(valuesForOperations), valuesForOperations, "*");
                         break;
                     case "5":
                         valuesForOperations = AskNumbers(2);
-                        ShowResult(Calculator.Power(valuesForOperations[0], valuesForOperations[1]));
+                        ShowResult(Calculator.Power(valuesForOperations[0], valuesForOperations[1]), valuesForOperations, "power");
                         break;
                     case "6":
                         valuesForOperations = AskNumbers(1);
-                        ShowResult(Calculator.Factorial(valuesForOperations[0]));
+                        ShowResult(Calculator.Factorial(valuesForOperations[0]),valuesForOperations,"factorial");
                         break;
                     case "7":
                         break;
@@ -62,10 +62,10 @@ namespace UiCalc
             Console.ReadKey();
         }
 
-        static void ShowResult(int res)
+        static void ShowResult(int res, int[] numbers, string operationType)
         {
             Console.Clear();
-            Console.WriteLine("The result of that operation is: " + res);
+            Console.WriteLine(ConstructExpression(numbers,operationType) + " = " + res);
             Console.Write("Press any key to continue...");
             Console.ReadKey();
         }
@@ -93,6 +93,47 @@ namespace UiCalc
             }
 
             return numbers;
+        }
+
+        static string ConstructExpression(int[] numbers, string typeOfOp)
+        {
+            string operation = "";
+
+            switch (typeOfOp)
+            {
+                case "power":
+                    operation = numbers[0].ToString() + "^" + numbers[1].ToString();
+                    break;
+                case "factorial":
+                    operation = numbers[0].ToString() + "!";
+                    break;
+                default:
+                    operation = numbers[0].ToString();
+                    for (int i = 1; i < numbers.Length; i++)
+                    {
+                        operation += " " + typeOfOp + " " + numbers[i].ToString();
+                    }
+                    break;
+            }
+            
+            return operation;
+        }
+
+        static void PrintMenu()
+        {
+            Console.WriteLine("***************************************");
+            Console.WriteLine("***********   UiCALC V0.1   ***********");
+            Console.WriteLine("***************************************");
+            Console.WriteLine("*********** 1 - Add         ***********");
+            Console.WriteLine("*********** 2 - Subtract    ***********");
+            Console.WriteLine("*********** 3 - Sum         ***********");
+            Console.WriteLine("*********** 4 - Multiply    ***********");
+            Console.WriteLine("*********** 5 - Power       ***********");
+            Console.WriteLine("*********** 6 - Factorial   ***********");
+            Console.WriteLine("*********** 7 - EXIT        ***********");
+            Console.WriteLine("***************************************");
+            Console.WriteLine("");
+            Console.Write("Please choose an option: ");
         }
     }
 }
